fbf967b62a69f2d2b3dc43f1dd9f1865ad992db9
Jamie Reynolds <jamus@jamus.co.uk>
2012-10-02 20:47:15 +0100

    Change of structure
    process.php is now the set-up and cron file for driving the CMS.
    functions.php is now a separate file for functions called.

--- includes/functions.php
+++ includes/functions.php
@@ -1,170 +1,74 @@
-<?php
+ <?php
 
-/// general setup  - Change optiosn below to suit
+/// Functions required for formatting etc
+ // 
 
-	include ('config.php');
-	
-//// Get tweets (API 1.1)
-
-
-$json = file_get_contents("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=50", true); //getting the file content
-$decode = json_decode($json, true); //getting the file content as array
-
-$timestamp = time();
-echo "time now: " . $timestamp;
  
-/// Get stats & bio
-$name = $decode[0][user][name];
-//$screen_name = $decode[0][user][screen_name]; // unrequired
-$description = $decode[0][user][description];
-$location = $decode[0][user][location];
-$url = $decode[0][user][url];
-$created_at = $decode[0][user][created_at];
-	// format date the way we want for credit
-	$created_year = strftime("%Y",strtotime($created_at));
-
-
-
-$followers_count = $decode[0][user][followers_count];
-$statuses_count = $decode[0][user][statuses_count];
-$friends_count = $decode[0][user][friends_count];
-$listed_count = $decode[0][user][listed_count];
-
-$profile_image_url = $decode[0][user][profile_image_url];
-
-
-
-$bio='';
-$stats='';
-$credits='';
-
-
-/////////////// BIO
-
-
-$bio.='<h1>'.$name.'</h1>
-		<h2><a href="http://twitter.com/#!/'.$twitteruser.'" target="_blank">@'.$twitteruser.'</a></h2>
-		<p>' . $description . '</p>';
-
-//echo $bio;
-
-		$handle=fopen("bio.php","w") or die("cannot open header.php");
-		if(fwrite($handle,$bio,strlen($bio)))
-		  echo "<p><strong>Bio updated</strong></p>";
-		fclose($handle);
-
-
-/////////////// STATS 
-$stats.='
-			
-	<ul >
-		<li ><a href="http://twitter.com/#!/'.$twitteruser.'" target="_blank">'.$statuses_count.'</a> Tweets made						
-		</li>
-		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/followers" target="_blank">'.$followers_count.'</a> Following me
-		</li>
-		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/following" target="_blank">'.$friends_count.'</a> Followed by me
-		</li>
-		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/lists/memberships" target="_blank">'.$listed_count.'</a> Lists feature me
-		</li>
-	</ul>';
-
-//echo $stats;
-
-
-		$handle=fopen("stats.php","w") or die("cannot open stats.php");
-		if(fwrite($handle,$stats,strlen($stats)))
-		  echo "<p><strong>Stats updated</strong></p>";
-		fclose($handle);
-
-
-
-/////////////// CREDITS 
-
-
-$credits.='<p>© ' . $created_year . ' - ' . date('Y', time()) . ' ' .$name.'</p>
-		<p>' . $location . '</p>';
-
-//echo $credits;
-
-		$handle=fopen("credits.php","w") or die("cannot open header.php");
-		if(fwrite($handle,$credits,strlen($credits)))
-		  echo "<p><strong>Credits updated</strong></p>";
-		fclose($handle);
-
-
-
-/////////////// TWEETS
-
-foreach ($keywords as &$keyword) {
-	 echo "<p><strong>Getting Tweets for #".$keyword."</strong><br>" ;
-		$count = count($decode); 
-		for($i=0;$i<$count;$i++){
-		//echo $decode[$i][text]."<br>";
-			if (strpos($decode[$i][text],'#'.$keyword.'') !== false) {
-    			//echo $decode[$i][text]."<br>";
-    				//get the id from entry
-					$id = $decode[$i][id];
-					//echo $id;
-					
-    				$tweet_date = $decode[$i][created_at];  
-    				
-    				//// calculate time difference
-    				echo '$tweet_date: ' . $tweet_date . '<br>';
-    				$tweet_stamp = strtotime($tweet_date);
-    				echo '$tweet_stamp:' . $tweet_stamp . "<br><br>";
-
-
-    				echo '$timestamp: ' . $timestamp . '<br>';
-    				echo strtotime($timestamp), "<br><br>";
-    				
-    				$diff = ($timestamp - $tweet_date);
-    				echo "difference: " . $diff;
-    				
-    				
-    				$tweet = $decode[$i][text];
-    			
-    				//remove <b> and </b> from the tweet. If you want to show bold keyword then you can comment this line
-					$tweet = str_replace(array("<b>", "</b>"), "", $tweet);
-			
-					// remove hash tag label
-					$tweet = str_replace(array(">@"), "class='atsign'>@", $tweet);
-			
-					// add class to remaing hash tag links
-					$tweet = str_replace(array("onclick"), "class='tag' onclick", $tweet);
-
-					// add target blank to llinks
-					$tweet = str_replace(array("<a"), "<a target=\"_blank\"", $tweet);
-					
-					//the result div that holds the information
-					$tweetcontents ='<article class="'.$keyword.' cols_2"><h2>'.$keyword.'</h2>
-					<p >'. $tweet .'</p>
-						<a href="http://twitter.com/#!/'.$twitteruser.'/statuses/'.$id.'" target="_blank" >
-						' . $tweet_date . '</a>
-							</article>';
-							
-					$handle=fopen("tweet_".$keyword.".php","w") or die("cannot open tweet_".$keyword.".php");
-					if(fwrite($handle,$tweetcontents,strlen($tweetcontents)))
-					//echo "Found one dated " . $tweet_date . "</p>";
-					fclose($handle);
-    			
-    				echo $tweetcontents;
-    				
-    			break; /// we don't want more than one so stop
-			} 
-			
+if(!function_exists(timeBetween))
+{
+	function timeBetween($start_date,$end_date)
+	{
+		$diff = $end_date-$start_date;
+ 		$seconds = 0;
+ 		$hours   = 0;
+ 		$minutes = 0;
+	
+		if($diff % 86400 <= 0){$days = $diff / 86400;}  // 86,400 seconds in a day
+		if($diff % 86400 > 0)
+		{
+			$rest = ($diff % 86400);
+			$days = ($diff - $rest) / 86400;
+     		if($rest % 3600 > 0)
+			{
+				$rest1 = ($rest % 3600);
+				$hours = ($rest - $rest1) / 3600;
+        		if($rest1 % 60 > 0)
+				{
+					$rest2 = ($rest1 % 60);
+           		$minutes = ($rest1 - $rest2) / 60;
+           		$seconds = $rest2;
+        		}
+        		else{$minutes = $rest1 / 60;}
+     		}
+     		else{$hours = $rest / 3600;}
 		}
-		
-		
-		 
-		
+
+		if($days > 365){$year = $year.' Over a year '; $month=false; $days = false; $hours = false; $minutes = false; $seconds = false;}
+		if($days > 30){$year = false; $month = $month.' Over a month '; $hours = false; $minutes = false; $seconds = false;}
+		if($days >= 2){$year = false; $month = false; $days = $days.' days '; $hours = false; $minutes = false; $seconds = false;}
+		if($days == 1){$year = false; $month = false; $days = $days.' day '; $hours = false; $minutes = false; $seconds = false;}
+		if($hours > 1){$year = false; $month = false; $days = false; $hours = $hours.' hours'; $minutes = false; $seconds = false;}
+		if($hours == 1){$year = false; $month = false; $year = false; $month = false; $days = false; $hours = $hours.' hour'; $minutes = false; $seconds = false;}
+		if($minutes > 1){$days = false; $hours = false; $minutes = $minutes.' minutes'; $seconds = false;}
+		//if($hours > 0){$hours = $hours.' hours, ';}
+		//else{$hours = false;}
+		//if($minutes > 0){$minutes = $minutes.' minutes, ';}
+		//else{$minutes = false;}
+		//$seconds = $seconds.' seconds'; // always be at least one second
+		//else {days = false; $hours = false; $minutes = false; $seconds = false;}
+
+		return $year . '' .$month . '' .$days.''.$hours.''.$minutes.''.$seconds . ' ago';
+	}
 }
 
 
+function create_tags($tweet) {
+
+	
+    preg_match_all('/#\S*\w/i', $tweet, $tags);
+    
+    $taglist = "";
+	foreach( $tags[0] as $tag ) {
+                $taglist .= '<li>'. $tag .'</li>';
+                }
+    //echo $taglist;
+	return $taglist;
+}
 
 
-/////////////// DONE LINK TO VIEW
-
-	echo '<a href="../">Ok. You\'re all set - Click here to view your 140cms page.</a>'; 
+ ?>
 
 
-?>
+
+
+
---
+++ includes/process.php
@@ -0,0 +1,173 @@
+<?php
+
+/// general setup  - Change optiosn below to suit
+
+	include ('config.php');
+	include ('functions.php');
+	
+//// Get tweets (API 1.1)
+
+
+$json = file_get_contents("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=50", true); //getting the file content
+$decode = json_decode($json, true); //getting the file content as array
+
+$rightnow = time();
+/// echo "time now: " . $rightnow;
+ 
+/// Get stats & bio
+$name = $decode[0][user][name];
+//$screen_name = $decode[0][user][screen_name]; // unrequired
+$description = $decode[0][user][description];
+$location = $decode[0][user][location];
+$url = $decode[0][user][url];
+$created_at = $decode[0][user][created_at];
+	// format date the way we want for credit
+	$created_year = strftime("%Y",strtotime($created_at));
+
+
+
+$followers_count = $decode[0][user][followers_count];
+$statuses_count = $decode[0][user][statuses_count];
+$friends_count = $decode[0][user][friends_count];
+$listed_count = $decode[0][user][listed_count];
+
+$profile_image_url = $decode[0][user][profile_image_url];
+
+
+
+$bio='';
+$stats='';
+$credits='';
+
+
+/////////////// BIO
+
+
+$bio.='<h1>'.$name.'</h1>
+		<h2><a href="http://twitter.com/#!/'.$twitteruser.'" target="_blank">@'.$twitteruser.'</a></h2>
+		<p>' . $description . '</p>';
+
+//echo $bio;
+
+		$handle=fopen("bio.php","w") or die("cannot open header.php");
+		if(fwrite($handle,$bio,strlen($bio)))
+		  echo "<p><strong>Bio updated</strong></p>";
+		fclose($handle);
+
+
+/////////////// STATS 
+$stats.='
+			
+	<ul >
+		<li ><a href="http://twitter.com/#!/'.$twitteruser.'" target="_blank">'.$statuses_count.'</a> Tweets made						
+		</li>
+		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/followers" target="_blank">'.$followers_count.'</a> Following me
+		</li>
+		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/following" target="_blank">'.$friends_count.'</a> Followed by me
+		</li>
+		<li ><a href="http://twitter.com/#!/'.$twitteruser.'/lists/memberships" target="_blank">'.$listed_count.'</a> Lists feature me
+		</li>
+	</ul>';
+
+//echo $stats;
+
+
+		$handle=fopen("stats.php","w") or die("cannot open stats.php");
+		if(fwrite($handle,$stats,strlen($stats)))
+		  echo "<p><strong>Stats updated</strong></p>";
+		fclose($handle);
+
+
+
+/////////////// CREDITS 
+
+
+$credits.='<p>© ' . $created_year . ' - ' . date('Y', time()) . ' ' .$name.'</p>
+		<p>' . $location . '</p>';
+
+//echo $credits;
+
+		$handle=fopen("credits.php","w") or die("cannot open header.php");
+		if(fwrite($handle,$credits,strlen($credits)))
+		  echo "<p><strong>Credits updated</strong></p>";
+		fclose($handle);
+
+
+
+/////////////// TWEETS
+
+foreach ($keywords as &$keyword) {
+	 echo "<p><strong>Getting Tweets for #".$keyword."</strong><br>" ;
+		$count = count($decode); 
+		for($i=0;$i<$count;$i++){
+		//echo $decode[$i][text]."<br>";
+			if (strpos($decode[$i][text],'#'.$keyword.'') !== false) {
+    			//echo $decode[$i][text]."<br>";
+    				//get the id from entry
+					$id = $decode[$i][id];
+					//echo $id;
+					
+    				$tweet_date = $decode[$i][created_at];  
+    				
+    				//// calculate time difference
+    				echo '$tweet_date: ' . $tweet_date . '<br>';
+    				$tweet_stamp = strtotime($tweet_date);
+    				//echo '$tweet_stamp:' . $tweet_stamp . "<br><br>";
+					//echo '$rightnow: ' . $rightnow . '<br>';
+    				//echo strtotime($rightnow), "<br><br>";
+    				
+    				echo 'Was posted ' . timeBetween($tweet_stamp, $rightnow).'<br>';
+    				
+    				$tweetedtime = timeBetween($tweet_stamp, $rightnow);
+    				
+    				$tweet = $decode[$i][text];
+
+    				$tags = create_tags($tweet);
+
+					
+    			
+    				//remove <b> and </b> from the tweet. If you want to show bold keyword then you can comment this line
+					//$tweet = str_replace(array("<b>", "</b>"), "", $tweet);
+			
+					// remove hash tag label
+					//$tweet = str_replace(array(">@"), "class='atsign'>@", $tweet);
+			
+					// add class to remaing hash tag links
+					//$tweet = str_replace(array("onclick"), "class='tag' onclick", $tweet);
+
+					// add target blank to llinks
+					//$tweet = str_replace(array("<a"), "<a target=\"_blank\"", $tweet);
+					
+					//the result div that holds the information
+					$tweetcontents ='<article class="'.$keyword.' cols_2"><h2>'.$keyword.'</h2>
+					<h3><a href="http://twitter.com/#!/'.$twitteruser.'/statuses/'.$id.'" target="_blank" >' . $tweetedtime .'</a></h3>
+					<p >'. $tweet .'</p>
+					<ul>' . $tags . '</ul>
+					</article>';
+							
+					$handle=fopen("tweet_".$keyword.".php","w") or die("cannot open tweet_".$keyword.".php");
+					if(fwrite($handle,$tweetcontents,strlen($tweetcontents)))
+					//echo "Found one dated " . $tweet_date . "</p>";
+					fclose($handle);
+    			
+    				echo $tweetcontents;
+    				
+    			break; /// we don't want more than one so stop
+			} 
+			
+		}
+		
+		
+		 
+		
+}
+
+
+
+
+/////////////// DONE LINK TO VIEW
+
+	echo '<a href="../">Ok. You\'re all set - Click here to view your 140cms page.</a>'; 
+
+
+?>
